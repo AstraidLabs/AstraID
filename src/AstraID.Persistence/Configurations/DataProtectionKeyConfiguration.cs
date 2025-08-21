35c72db0625caf56023b64f1ac30a56047ed6cb7
AstraidLabs <46496187+AstraidLabs@users.noreply.github.com>
2025-08-21 10:22:36 +0200

    Implement IDataProtectionKeyContext

--- src/AstraID.Persistence/Configurations/DataProtectionKeyConfiguration.cs
+++ src/AstraID.Persistence/Configurations/DataProtectionKeyConfiguration.cs
@@ -1,4 +1,4 @@
-using AstraID.Domain.Entities;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
